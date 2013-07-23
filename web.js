ce6dc6f35feae0702d82cd4accfa73f9f3527f11
geoxdoug <geodoug@gmail.com>
2013-07-23 17:47:56 +0000

    modified param of buf.toString

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 var fs = require('fs');
 var content = fs.readFileSync('index.html','utf8');
-response.send(buf.toString());
+response.send(buf.toString('utf8', 0, len));
 });
 
 var port = process.env.PORT || 5000;
